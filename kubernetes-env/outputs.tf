3a45cd5363663c2b8d672a8f9adf1d9f2658ae40
Adam Goss <adamgoss@protonmail.com>
2024-02-09 09:49:57 +0000

    kubernetes-env

---
+++ kubernetes-env/outputs.tf
@@ -0,0 +1,14 @@
+output "master-ip" {
+  description = "IP of master node"
+  value       = "10.0.6.200"
+}
+
+output "worker01-ip" {
+  description = "IP of worker node 1"
+  value       = "10.0.6.201"
+}
+
+output "worker02-ip" {
+  description = "IP of worker node 2"
+  value       = "10.0.6.202"
+}
